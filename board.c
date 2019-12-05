0559d529516bb2df2ac5fcb6c65d41c08b592095
Imad-HILI <56233782+Imad-HILI@users.noreply.github.com>
2019-12-06 00:08:39 +0100

    test struct piece

--- board.c
+++ board.c
@@ -6,40 +6,36 @@ void initiale(carre **A){
     int i,j;
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
-            A[i][j].up=NULL;
-            A[i][j].down=NULL;
-            A[i][j].left=NULL;
-            A[i][j].right=NULL;
-            A[i][j].roi=0;
+            A[i][j].up->type=NULL;       A[i][j].up->color=NULL;
+            A[i][j].down->type=NULL;     A[i][j].down->color=NULL;
+            A[i][j].left->type=NULL;     A[i][j].left->color=NULL;
+            A[i][j].right->type=NULL;    A[i][j].right->color=NULL;
+            A[i][j].roi->type=NULL;      A[i][j].roi->color=NULL;
             if(i==0){
-                A[i][j].up=NULL;
-                A[i][j].down=PION;
-                A[i][j].left=DAME;
-                A[i][j].right=DAME;
-                A[i][j].roi=0;
-                if(j==3) A[i][j].roi=1;
+                A[i][j].down->type=PION;        A[i][j].down->color=BLANCHE;
+                A[i][j].left->type=DAME;        A[i][j].left->color=BLANCHE;
+                A[i][j].right->type=DAME;       A[i][j].right->color=BLANCHE;
+                if(j==3){
+                    A[i][j].roi->type=ROI;      A[i][j].roi->color=BLANCHE;
+                }
             }
             if(i==1){
-                A[i][j].up=PION;
-                A[i][j].down=NULL;
-                A[i][j].left=PION;
-                A[i][j].right=PION;
-                A[i][j].roi=0;
+                A[i][j].up->type=PION;       A[i][j].up->color=BLANCHE;
+                A[i][j].left->type=PION;     A[i][j].left->color=BLANCHE;
+                A[i][j].right->type=PION;    A[i][j].right->color=BLANCHE;
             }
             if(i==5){
-                A[i][j].up=NULL;
-                A[i][j].down=PION;
-                A[i][j].left=PION;
-                A[i][j].right=PION;
-                A[i][j].roi=0;
+                A[i][j].down->type=PION;     A[i][j].down->color=NOIRE;
+                A[i][j].left->type=PION;     A[i][j].left->color=NOIRE;
+                A[i][j].right->type=PION;    A[i][j].right->color=NOIRE;
+                if(j==3){
+                    A[i][j].roi->type=ROI;      A[i][j].roi->color=BLANCHE;
+                }
             }
             if(i==6){
-                A[i][j].up=PION;
-                A[i][j].down=NULL;
-                A[i][j].left=DAME;
-                A[i][j].right=DAME;
-                A[i][j].roi=0;
-                if(j==3) A[i][j].roi=1;
+                A[i][j].up->type=PION;           A[i][j].up->color=NOIRE;
+                A[i][j].left->type=DAME;         A[i][j].left->color=NOIRE;
+                A[i][j].right->type=DAME;        A[i][j].right->color=NOIRE;
             }
 
         }
@@ -49,16 +45,16 @@ void affichage(carre **A){
     int i,j;
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
-            printf("  %c  ",A[i][j].up);
+            printf("  %c  ",A[i][j].up->type);
         }printf("\n");
         for(j=0;j<N;j++){
-            printf("%c ",A[i][j].left);
-            printf("%d ",A[i][j].roi);
-            printf("%c",A[i][j].right);
+            printf("%c ",A[i][j].left->type);
+            printf("%d ",A[i][j].roi->type);
+            printf("%c",A[i][j].right->type);
 
         }printf("\n");
         for(j=0;j<N;j++){
-            printf("  %c  ",A[i][j].down);
+            printf("  %c  ",A[i][j].down->type);
         }printf("\n");
     }
 }
--- board.h
+++ board.h
@@ -1,14 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
 
 #define PION 'p'
 #define DAME 'd'
+#define ROI 'R'
+#define BLANCHE 'B'
+#define NOIRE 'N'
 #define N 7
 
 typedef struct {
-    char *up;
-    char *down;
-    char *left;
-    char *right;
-    int roi;
+    char *type;
+    char *color;
+}piece;
+typedef struct {
+    piece *up;
+    piece *down;
+    piece *left;
+    piece *right;
+    piece *roi;
 } carre;
+
+void initiale(carre **);
+void affichage(carre **);
+#endif // BOARD_H_INCLUDED
