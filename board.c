255f1254e70b2cb2b7a7d5773ed2965ee4884b7e
Imad-HILI <56233782+Imad-HILI@users.noreply.github.com>
2019-12-10 17:28:07 +0100

    affichage sur terminale

--- board.c
+++ board.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "board.h"
+#include<string.h>
 
 void initialplateau(piece **A){
     int i,j;
@@ -14,7 +15,7 @@ void initialplateau(piece **A){
                 A[i][j].firstmove = INTERDIT;
             }
 
-            if (i==1) { 
+            if (i==1) {
                 if (j==7) {
                     A[i][j].type = ROI; A[i][j].color=NOIRE;
                     A[i][j].firstmove = VIDE;
@@ -56,11 +57,66 @@ void initialplateau(piece **A){
 }
 
 void affichage(piece **A) {
-    int i,j;
+    int i,j,k;
     for (i=0;i<DIM_PLAT;i++){
+        if(i%2==1){ /*juste pour grandir la case royale*/
+            for(j=0;j<DIM_PLAT;j++){
+                if(j==0){
+                    if(j%2==0) printf("  |  |");
+                    if(j%2==1) printf("        ");
+                }else{
+                    if(j%2==0) printf("|  |");
+                    if(j%2==1) printf("      ");
+            }}printf("\n");
+        }/*termine ici*/
+        /*affichage des cases horizontale*/
         for (j=0;j<DIM_PLAT;j++){
-            printf("  |%c/%c|  ",A[i][j].type,A[i][j].color);
-        }printf("\n");
-    }
+                if(i%2==0){
+                    if(j==0){
+                        if(j%2==0) printf("H%d|**|",(i+1)/2+1);
+                        if(j%2==1) {
+                            if(A[i][j].type!=VIDE && A[i][j].color!=VIDE) printf("__%c%c__",A[i][j].type,A[i][j].color);
+                            else printf("______");
+
+                    }
+                    }
+                    else{
+                        if(j%2==0) printf("|**|");
+                        if(j%2==1) {
+                            if(A[i][j].type!=VIDE && A[i][j].color!=VIDE) printf("__%c%c__",A[i][j].type,A[i][j].color);
+                            else printf("______");
+
+                    }
+                    }
+
+                }
+                /*affichage contenu des cases royales et les dames a coté*/
+                if(i%2==1){
+                    if(j==0){
+                        if(j%2==0) printf("%c |%c%c|",(i+65)-(i+1)/2,A[i][j].type,A[i][j].color);
+                    }
+                    else{
+                        if(j%2==0) printf("|%c%c|",A[i][j].type,A[i][j].color);
+                        if(j%2==1) printf("  %c%c  ",A[i][j].type,A[i][j].color);
+                    }
+
+                }
+    }printf("\n");
+    /*affichage des cases terminer*/
+    if(i%2==1){ /*juste pour grandir la case royale*/
+            for(j=0;j<DIM_PLAT;j++){
+                if(j==0){
+                    if(j%2==0) printf("  |  |");
+                    if(j%2==1) printf("  ______");
+                }else{
+                    if(j%2==0) printf("|  |");
+                    if(j%2==1) printf("______");
+            }}printf("\n");
+        } /*termine ici*/
 }
-                
+/* Affichage des indices des colonnes*/
+for(j=0;j<DIM_PLAT;j++){
+    if(j%2==0) printf("   V%d ",(j+1)/2+1);
+    if(j%2==1) printf("  %d ",(j+1)/2);
+}
+}
